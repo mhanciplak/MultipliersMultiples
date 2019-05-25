00abc69aecd09864a91d70862a32d885ca6bd259
Dogac Akyildiz <ndakyildiz@gmail.com>
2019-05-26 01:39:27 +0300

    Scene 3 Düzenlendi.

---
+++ MultipliersMultiplesMHC/Assets/Scripts/Scene 3/HouseScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HouseScript : MonoBehaviour
+{
+    public GameObject Tower, SuccessPanel, WrongPanel;
+    public InputField HouseNumber, WindowNumber, TotalNumber;
+
+    public void Check()
+    {
+        if (HouseNumber.text == "4" && WindowNumber.text == "2"
+            && TotalNumber.text == "8")
+        {
+            StartCoroutine(WorkForSuccess());
+        }
+        else
+        {
+            WrongPanel.SetActive(true);
+        }
+    }
+
+    IEnumerator WorkForSuccess()
+    {
+        Tower.SetActive(true);
+
+        yield return new WaitForSeconds(1.5F);
+
+        SuccessPanel.SetActive(true);
+    }
+}
