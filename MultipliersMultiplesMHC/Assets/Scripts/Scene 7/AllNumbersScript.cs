56b3e660cba0963eac8315d4bb9b7f4945a470ae
Dogac Akyildiz <ndakyildiz@gmail.com>
2019-05-26 07:34:12 +0300

    Scene 7 düzenlendi.

---
+++ MultipliersMultiplesMHC/Assets/Scripts/Scene 7/AllNumbersScript.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllNumbersScript : MonoBehaviour
+{
+    public static int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+    public static int trueCount;
+
+    private GameObject Code, KalburImage;
+    private bool isPrime;
+    private Sprite RedSprite, GreenSprite;
+
+    private bool isInPrimes(int num)
+    {
+        foreach (var item in primes)
+            if (num == item)
+                return true;
+
+        return false;
+    }
+
+    private void SetObjects()
+    {
+        Code = GameObject.Find("Code");
+        KalburImage = GameObject.Find("KalburImage");
+    }
+
+    private void SetPrime()
+    {
+        if (isInPrimes(int.Parse(this.gameObject.name)))
+            isPrime = true;
+        else
+            isPrime = false;
+    }
+
+    private void AssignSprites()
+    {
+        GreenSprite = GameObject.Find("Green").GetComponent<SpriteRenderer>().sprite;
+        RedSprite = GameObject.Find("Red").GetComponent<SpriteRenderer>().sprite;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetObjects();
+        SetPrime();
+        AssignSprites();
+    }
+
+    private void SetSprite()
+    {
+        if (this.isPrime)
+            this.GetComponent<SpriteRenderer>().sprite = GreenSprite;
+        else
+            this.GetComponent<SpriteRenderer>().sprite = RedSprite;
+    }
+
+    private void WorkForRights()
+    {
+        if (!this.isPrime)
+            Code.GetComponent<RightsScript>().Decrease();
+    }
+
+    private void SetTrueCount()
+    {
+        if (this.isPrime) ++trueCount;
+    }
+
+    private void CheckTrueCount()
+    {
+        if (trueCount == 9)
+        {
+            KalburImage.SetActive(false);
+            Code.GetComponent<RightsScript>().PlayKalburGif();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        SetSprite();
+
+        SetTrueCount();
+        CheckTrueCount();
+
+        WorkForRights();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MultipliersMultiplesMHC/Assets/Scripts/Scene 7/RightsScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RightsScript : MonoBehaviour
+{
+    public TextMeshProUGUI rightCount;
+    public GameObject RestartPanel, SuccessPanel, KalburGif;
+    private GameObject KalburImage;
+
+    void Start()
+    {
+        KalburImage = GameObject.Find("KalburImage");
+        rightCount.text = "3";
+    }
+
+    private bool isZero()
+    {
+        return rightCount.text == "0";
+    }
+
+    public void Decrease()
+    {
+        rightCount.text = (int.Parse(rightCount.text) - 1).ToString();
+
+        if (isZero())
+        {
+            KalburImage.SetActive(false);
+            RestartPanel.SetActive(true);
+        }
+    }
+
+    public void PlayKalburGif()
+    {
+        KalburGif.SetActive(true);
+    }
+
+    IEnumerator ShowSuccessPanel()
+    {
+        yield return new WaitForSeconds(15);
+
+        SuccessPanel.SetActive(true);
+    }
+}
